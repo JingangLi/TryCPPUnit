b89c5f7e6c237c7f6b856d5295d5f3be16e48e96
AlexanderLi <ljg_emails@sina.com>
2012-08-31 22:08:45 +0800

    Init project for cppunit

---
+++ cppunit/src/cppunit.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cppunit.cpp
+// Author      : Jingang Li
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "cppunit" << endl; // prints cppunit
+	return 0;
+}
